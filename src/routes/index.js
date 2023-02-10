04fec6c418752bb8e0165346c2db607b4516ab55
constantin_m <cmitrokhin@applaudostudios.dev>
2023-02-10 14:16:19 -0300

    Rutas de acceso CRUD

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 /* eslint-disable new-cap */
 const {Router} = require('express');
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const ingredientsRouter = require("./ingredients")
+const recipesRouter = require("./recipes");
 
 const router = Router();
 
 // Configurar los routers
-
+router.use("/ingredients", ingredientsRouter);
+router.use("/recipes", recipesRouter);
 
 module.exports = router;
