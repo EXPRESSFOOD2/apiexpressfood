520123ed6d30f87c2a545cfa438072049d34bf0c
Eduar2arias <Eduar2_arias@hotmail.com>
2023-02-13 13:22:06 -0500

    rutas recipe / method put - delete / middleware / recipe controller

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const router = Router();
-const { validateRecipePost, validateRecipeGet } = require("../middleware/recipe-middleware")
+const { validateRecipePost, validateRecipeGet , validateIdRecipe, validateRecipeData } = require("../middleware/recipe-middleware")
 const { postRecipe } = require("../controllers/recipe-post-controller");
-const { getAllRecipes } = require("../controllers/recipe-get-controller")
+const { getAllRecipes } = require("../controllers/recipe-get-controller");
+const { deleteRecipe } = require('../controllers/recipe-delete-controller');
+const { updateRecipe } = require('../controllers/recipe-put-controller');
 
 router.post("/", async (req, res) => {
     const { name, details, produced_amount, type_measure, ingredArray } = req.body;
@@ -30,11 +32,27 @@ router.get("/", async (req, res) => {
 })
 
 router.delete("/", async (req, res) => {
+    const { id_recipe  } = req.body
     try {
         /* TODO */
-        return res.status(200).json( "Recipes DELETE" )
+        validateIdRecipe(  id_recipe )
+        const result = await deleteRecipe(id_recipe)
+        return res.status(200).json(result)
     } catch (error) {
         return res.status(400).json({ error: error.message })
+    }
+})
+
+
+router.put("/", async (req ,res )=> {
+    const { id_recipe , name , detail} = req.body
+
+    try {
+        validateRecipeData(name , detail)
+        const result = await updateRecipe ( id_recipe , name , detail )
+        res.status(200).json(result)
+    } catch (error) {   
+        res.status(404).json(error.message)
     }
 })
 
