5d81e7930776272ba1f9ed6525e7597445886cd2
Eduar2arias <Eduar2_arias@hotmail.com>
2023-02-14 18:53:24 -0500

    test / user - menu

---
+++ src/models/utils/MenuItem-ErrorMSGs.js
@@ -0,0 +1,10 @@
+module.exports = {
+    
+        ERROR_NAME:"Invalid Menu Name: Can't be Empty",
+        ERROR_PRICE :{
+            PRICE_IS_STRING :"Must be a Numeric: 0 or bigger",
+            ERROR_PRICE_LESS_0 : "Must be a Numeric: 0 or bigger",
+        } ,
+        ERROR_CREATE_NEW_MENU_ITEM:"llave duplicada viola restricción de unicidad «MenuItems_name_key»"
+    
+}
