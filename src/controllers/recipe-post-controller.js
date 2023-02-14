a37480f16933c2a52e0d2207a63114a4737b396c
constantin_m <cmitrokhin@applaudostudios.dev>
2023-02-14 02:22:20 -0300

    Adds ingedients_all to Ingredient Model

--- src/controllers/recipe-post-controller.js
+++ src/controllers/recipe-post-controller.js
@@ -1,22 +1,26 @@
 const { Recipe, Ingredient, IngredientsRecipes } = require('../db');
 
 const postRecipe = async (name, details, produced_amount, type_measure, ingredArray) => {
-     //Ingredient.create({name: "Papa", layer: 0, type_measure: "gr"})
-     //Ingredient.create({name: "Manteca purificada", layer: 1, type_measure: "gr"})
+    Ingredient.create({name: "Papa", layer: 0, type_measure: "gr"})
+    Ingredient.create({name: "Manteca purificada", layer: 1, type_measure: "gr"})
     // Ingredient.create({name: "Papa Negra", layer: 0, type_measure: "gr"})
     // Ingredient.create({name: "Manteca purificada plus", layer: 1, type_measure: "gr"})
                                                              
     let newRecipe = await Recipe.create({name, details, produced_amount})
     let layer = processLayer(ingredArray)
-    createIngredientsRecipes({RecipeId: newRecipe.id, ingredArray})
-    Ingredient.create({name, layer, type_measure})
+    const ingredients_all = JSON.stringify( await createIngredientsRecipes({RecipeId: newRecipe.id, ingredArray}))
+    console.log(ingredients_all)
+    Ingredient.create({name, layer, type_measure, ingredients_all})
     return newRecipe;
 }
 const createIngredientsRecipes = async ({RecipeId, ingredArray}) => {
+    let retorno = [];
     const result = ingredArray.map( ing => {
+        retorno.push({ingedient_id: ing.id, per_recipe: ing.per_recipe * ((100 + ing.waste_rate) / 100 )})
         return {RecipeId, IngredientId: ing.id, waste_rate: ing.waste_rate, per_recipe: ing.per_recipe}
     } )
-    IngredientsRecipes.bulkCreate(result)
+    await IngredientsRecipes.bulkCreate(result)
+    return retorno;
 }
 
 const processLayer = (ingredArray) => {
