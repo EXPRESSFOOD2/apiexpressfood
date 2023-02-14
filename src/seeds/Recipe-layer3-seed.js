baa5179ae0f0529ffcaf503841438cb43fc6f5b6
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-02-14 18:57:45 -0500

    fix layer property in bulkcreate seeders

--- src/seeds/Recipe-layer3-seed.js
+++ src/seeds/Recipe-layer3-seed.js
@@ -13,21 +13,21 @@ module.exports = async function() {
     await Ingredient.create({name: "Combo para ti solito", layer: 3, type_measure: "un", ingredients_all: JSON.stringify([
         {id: 13, name: "Pan Hamburguesa", amount: 1},
     
-        {id: 12, name: "Carne Molida", amount: 80},
-        {id: 3, name: "Huevo", amount: 0.6},
-        {id: 7, name: "Cebolla", amount: 26},
-        {id: 5, name: "Sal", amount: 0.4},
-        {id: 10, name: "Pimienta", amount: 0.6},
+        {id: 12, name: "Carne Molida", layer:0, amount: 80},
+        {id: 3, name: "Huevo", layer:0, amount: 0.6},
+        {id: 7, name: "Cebolla", layer:0, amount: 26},
+        {id: 5, name: "Sal", layer:0, amount: 0.4},
+        {id: 10, name: "Pimienta", layer:0, amount: 0.6},
 
-        {id: 14, name: "Ketchup", amount: 20},
-        {id: 15, name: "Mayonesa", amount: 20},
-        {id: 8, name: "Tomate", amount: 60},
+        {id: 14, name: "Ketchup", layer:0, amount: 20},
+        {id: 15, name: "Mayonesa", layer:0,  amount: 20},
+        {id: 8, name: "Tomate", layer:0, amount: 60},
     
-        {id: 9, name: "Lechuga", amount: 50},
-        {id: 18, name: "Queso Cheddar", amount: 70},
-        {id: 19, name: "Papa", amount: 100},
-        {id: 17, name: "Aceite de Oliva", amount: 0.6},
-        {id: 20, name: "Gaseosa", amount: 1},
+        {id: 9, name: "Lechuga", layer:0, amount: 50},
+        {id: 18, name: "Queso Cheddar", layer:0, amount: 70},
+        {id: 19, name: "Papa", layer:0, amount: 100},
+        {id: 17, name: "Aceite de Oliva", layer:0, amount: 0.6},
+        {id: 20, name: "Gaseosa", layer:0, amount: 1},
       ])})
     await Recipe.create({   name: "Pizza de queso familiar",
     details: "Deliciosa Pizza Tamaño familiar, con queso mozzarella, tomate y cebolla",
@@ -35,13 +35,13 @@ module.exports = async function() {
     type_measure: "un" })
     await Ingredient.create({name: "Pizza de queso familiar", layer: 2, type_measure: "un", ingredients_all: JSON.stringify([
         {id: 2, name: "Harina", amount: 0.5128},
-    {id: 16, name: "Levadura Fresca", amount: 0.0467},
-    {id: 1, name: "Agua", amount: 0.3364},
-    {id: 5, name: "Sal", amount: 0.0186},
-    {id: 17, name: "Aceite de Oliva", amount: 0.0373},
-    {id: 11, name: "Queso Mozzarella", amount: 500},
-    {id: 8, name: "Tomate", amount: 200},
-    {id: 7, name: "Cebolla", amount: 300},
+    {id: 16, name: "Levadura Fresca", layer:0,  amount: 0.0467},
+    {id: 1, name: "Agua", layer:0, amount: 0.3364},
+    {id: 5, name: "Sal",layer:0, amount: 0.0186},
+    {id: 17, name: "Aceite de Oliva",layer:0, amount: 0.0373},
+    {id: 11, name: "Queso Mozzarella",layer:0, amount: 500},
+    {id: 8, name: "Tomate", layer:0, amount: 200},
+    {id: 7, name: "Cebolla",layer:0, amount: 300},
         
     ])})
 
