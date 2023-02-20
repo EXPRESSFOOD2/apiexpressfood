6fb420bbef6d8a640f15dc142336e3663705a769
constantin_m <cmitrokhin@applaudostudios.dev>
2023-02-20 17:20:11 -0300

    Adds url validation

--- src/middleware/getImageFileURL.js
+++ src/middleware/getImageFileURL.js
@@ -1,8 +1,10 @@
 const {cloudinary} = require("../utils/cloudinary")
+const URL_REGEX = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 
 const getImageFileURL = async (req,res) => {
     try {
         const { imageStr} = req.body;
+        if ( URL_REGEX.test(imageStr) ) return res.status(200).json( imageStr );
         const uploadedResponse = await cloudinary.uploader.upload(imageStr, {upload_preset: process.env.CR_DEFAULT_FOLDER})
         return res.status(200).json( uploadedResponse.secure_url );
     } catch (error) {
