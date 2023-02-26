12aa89aad0f786de30bc90490492df96b2909e58
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-02-25 19:31:35 -0500

    add env variables

---
+++ src/controllers/Payments/payments-success-controler.js
@@ -0,0 +1,11 @@
+// const request = require('request')
+const { Orders } = require("../../db");
+
+
+
+const paymentsSuccessProcess = async (payparams) => {
+  //! GENERAR LA LOGICA DE LAS ORDENES ESTE CONTROLADOR RECIBE POR PARAMETROS
+  //! EL ID DEL PAGO Y EL ESTATUS
+};
+
+module.exports = { paymentsSuccessProcess };
