ba8706a3680c5b276adda6bfe6a9b803ba209163
constantin_m <cmitrokhin@applaudostudios.dev>
2023-02-26 14:36:36 -0300

    Resolver problemas de Async & error ortografico

--- src/controllers/Payments/payments-failure-controler.js
+++ src/controllers/Payments/payments-failure-controler.js
@@ -1,6 +1,6 @@
 const paymentsFailureProcess = async (failureResponse) => {
   let redirectUrl = process.env.CUSTOMER_DEV_URL_FAILURE || process.env.CUSTOMER_DEPLOY_URL_FAILURE;
-  return `${redirectUrl}/${failureResponse}`
+  return await `${redirectUrl}${failureResponse}`
 };
 
 module.exports = { paymentsFailureProcess };
