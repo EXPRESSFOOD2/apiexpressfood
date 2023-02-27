6a22881a52fe5325830e5d00a5cd1c9c0a0c1aac
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-02-26 23:01:10 -0500

    Update sendActivationEmail.js

--- src/controllers/htmlMessageMail/sendActivationEmail.js
+++ src/controllers/htmlMessageMail/sendActivationEmail.js
@@ -4,24 +4,31 @@ const path = require("path");
 const htmlPath = path.join(__dirname, "message.html");
 const html = fs.readFileSync(htmlPath, "utf8");
 
-
-const sendActivationEmail = (email) => {
+const sendActivationEmail = (email, others ) => {
   //CODIGO QUE ENVIA CORREO AL CLIENTE PARA LA ACTIVACION DE LA CUENTA
   const transporter = nodemailer.createTransport(
-    "smtps://expressfoodhenry@gmail.com:ngvootjfbrkbefub@smtp.gmail.com"
+    "smtps://spacefoodhenry@gmail.com:eniffmyycbcjrjeg@smtp.gmail.com"
   );
 
-  const mailOptions = {
+  const mailOptionsRegistration = {
     from: "ExpressFood",
     to: email,
-    subject: "Activacion de cuenta",
+    subject: "Confirmacion de Registro",
     html: html,
    
   };
 
+  const mailOptionsSuccesPayment = {
+    from: "ExpressFood",
+    to: email,
+    subject: `Numero de orden ${others}`,
+    html:html ,
+   
+  };
+
+others ? mailOptions = mailOptionsSuccesPayment : mailOptions = mailOptionsRegistration
   transporter.sendMail(mailOptions, (err, info) => {
-    if (err) console.log(err);
+    // if (err) console.log(err);
+  
   });
 };
-
-module.exports = { sendActivationEmail };
