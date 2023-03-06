3340097db49cc9747a85e3ec7410aaefe618f848
constantin_m <cmitrokhin@applaudostudios.dev>
2023-03-06 12:12:13 -0300

    totalSold a get/menu/:id devuelve total ordenes vendidas

--- src/controllers/menuItem/menuItem-get_controller.js
+++ src/controllers/menuItem/menuItem-get_controller.js
@@ -1,5 +1,5 @@
 const { MenuItem, Ingredient, Tag, Review, conn} = require("../../db");
-
+//[ "Sin Pagar", "En Progreso", "Cancelada", "Lista", "Entregada" ]
 
 const menuItemsGetController = async (store_id) => {
   const result = await MenuItem.findAll({
@@ -21,7 +21,7 @@ const menuItemsGetRecommendedController = async (store_id) => {
     include: [{ model: Tag, attributes: ["name"] }, { model: Ingredient }],
     attributes: {
       exclude: ["createdAt", "updatedAt", "deletedAt"],
-      include: [[conn.literal('(SELECT COALESCE(AVG("Reviews"."rating"), 0) FROM "Reviews" WHERE "MenuItem"."id" = "Reviews"."MenuItemId")'), 'rating']]
+      include: [[conn.literal('(SELECT COALESCE(AVG("Reviews"."rating"), 0) FROM "Reviews" WHERE "MenuItems"."id" = "Reviews"."MenuItemId")'), 'rating']]
     },
   });
   return filterMenuItems(result);
@@ -55,9 +55,13 @@ const menuItemsGetByIdController = async (id, store_id) => {
     include: [{ model: Tag, attributes: ["name"] }, { model: Ingredient }],
     attributes: {
       exclude: ["createdAt", "updatedAt", "deletedAt"],
-      include: [[conn.literal('(SELECT COALESCE(AVG("Reviews"."rating"), 0) FROM "Reviews" WHERE "MenuItem"."id" = "Reviews"."MenuItemId")'), 'rating']]
+      include: [
+        [conn.literal('(SELECT COALESCE(AVG("Reviews"."rating"), 0) FROM "Reviews" WHERE "MenuItem"."id" = "Reviews"."MenuItemId")'), 'rating'],
+        [conn.literal('(SELECT COALESCE(SUM("OrdersMenus"."quantity"), 0) FROM "Orders" INNER JOIN "OrdersMenus" ON "Orders"."order_id" = "OrdersMenus"."OrderId" WHERE "Orders"."status" = \'Entregada\' AND "OrdersMenus"."MenuItemId" = "MenuItem"."id")'), 'totalSold']
+      ]
     },
   });
+  return result
   return filterMenuItems([result])[0];
 };
 
