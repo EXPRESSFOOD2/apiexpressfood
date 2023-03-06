b672dad6e6fc02f1891dcf5625c244fdb86a8757
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-03-05 20:34:11 -0500

    Update order-get.js

--- src/routes/orders/order-get.js
+++ src/routes/orders/order-get.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const { processOrderGet, processOrderGetById } = require("../../middleware/order_middleware")
 
-router.get("/", processOrderGet );
-router.get("/:id", processOrderGetById );
+router.post("/", processOrderGet );
+router.post("/:id", processOrderGetById );
 
 module.exports = router;
