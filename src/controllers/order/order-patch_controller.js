b21354391b5bda15823e67607333085e37922ccb
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-03-07 12:00:45 -0500

    add catch to patch controller to view at console if sendmail fail

--- src/controllers/order/order-patch_controller.js
+++ src/controllers/order/order-patch_controller.js
@@ -5,19 +5,26 @@ const { sendEmail } = require("../htmlMessageMail/sendActivationEmail");
 const orderPatchController = async (id, store_id, status) => {
   //! TODO
   // Considerar agregar Construccion de un objeto a pasar para la modificacion
-  const result = await Order.update({ status }, { where: { id, store_id } });
+  try {
+    const result = await Order.update({ status }, { where: { id, store_id } });
 
-  const orderDataToSendMail = await Order.findOne({ where: { order_id: id } });
-  if (status === ORDER_STATUS[4]) {
-    const email = orderDataToSendMail.dataValues.client_data.email;
-    const orderCode = orderDataToSendMail.dataValues.code;
-    const orderId = orderDataToSendMail.dataValues.id;
+    const orderDataToSendMail = await Order.findOne({ where: { order_id: id } });
+    if (status === ORDER_STATUS[4]) {
+      const email = orderDataToSendMail.dataValues.client_data.email;
+      const orderCode = orderDataToSendMail.dataValues.code;
+      const orderId = orderDataToSendMail.dataValues.id;
+  
+      sendEmail(email, orderCode, orderId);
+      return result;
+    } else {
+      return result;
+    }
 
-    sendEmail(email, orderCode, orderId);
-    return result;
-  } else {
-    return result;
+  } catch (error) {
+    console.log(error.message);
   }
+
+ 
 };
 
 module.exports = {
