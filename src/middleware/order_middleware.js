3dcd2a844714db470020f1e34da4e988bc36796f
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-03-09 06:33:11 -0500

    hacer que review funcione

--- src/middleware/order_middleware.js
+++ src/middleware/order_middleware.js
@@ -9,18 +9,18 @@ const processOrderPrediction = async (req, res) => {
         //! Remastered !//
         const origin = req.headers.origin;
         let store_id = "";
-        if ( origin === process.env.HEADERS_STORE_ORIGIN_DEPLOY){
+        if (origin === process.env.HEADERS_STORE_ORIGIN_DEPLOY) {
             const token = req.headers.token;
             const user_id = req.headers.id;
-            if ( !token )  throw Error('AccessToken doesnt exist');
-            if ( !await validateToken(user_id, token ) ) throw Error("Token is invalid or expired, Please log in again.")
+            if (!token) throw Error('AccessToken doesnt exist');
+            if (!await validateToken(user_id, token)) throw Error("Token is invalid or expired, Please log in again.")
             store_id = await getStoreIdByUserId(user_id);
-        }else throw new Error("Access Denied")
+        } else throw new Error("Access Denied")
 
         const { toPredict } = req.body;
         //! Validar si todos los elem son del mismo store
-        const result  = await orderGetPredictionController( toPredict )
-        return res.status(200).json( result )
+        const result = await orderGetPredictionController(toPredict)
+        return res.status(200).json(result)
     } catch (error) {
         return res.status(400).json({ error: error.message })
     }
@@ -32,17 +32,17 @@ const processOrderGetBalance = async (req, res) => {
         const origin = req.headers.origin;
         console.log(origin)
         let store_id = "";
-        if ( origin === process.env.HEADERS_STORE_ORIGIN_DEPLOY){
+        if (origin === process.env.HEADERS_STORE_ORIGIN_DEPLOY) {
             const token = req.headers.token;
             const user_id = req.headers.id;
-            if ( !token )  throw Error('AccessToken doesnt exist');
-            if ( !await validateToken(user_id, token ) ) throw Error("Token is invalid or expired, Please log in again.")
+            if (!token) throw Error('AccessToken doesnt exist');
+            if (!await validateToken(user_id, token)) throw Error("Token is invalid or expired, Please log in again.")
             store_id = await getStoreIdByUserId(user_id);
-        }else throw new Error("Access Denied")
-console.log(store_id)
+        } else throw new Error("Access Denied")
+        console.log(store_id)
         const { startDate, endDate } = req.body;
-        const result  = await orderGetBalanceController( store_id, startDate, endDate )
-        return res.status(200).json( result )
+        const result = await orderGetBalanceController(store_id, startDate, endDate)
+        return res.status(200).json(result)
     } catch (error) {
         return res.status(400).json({ error: error.message })
     }
@@ -53,19 +53,19 @@ const processOrderPatch = async (req, res) => {
         //! Remastered !//
         const origin = req.headers.origin;
         let store_id = "";
-        if ( origin === process.env.HEADERS_STORE_ORIGIN_DEPLOY){
+        if (origin === process.env.HEADERS_STORE_ORIGIN_DEPLOY) {
             const token = req.headers.token;
             const user_id = req.headers.id;
-            if ( !token )  throw Error('AccessToken doesnt exist');
-            if ( !await validateToken(user_id, token ) ) throw Error("Token is invalid or expired, Please log in again.")
+            if (!token) throw Error('AccessToken doesnt exist');
+            if (!await validateToken(user_id, token)) throw Error("Token is invalid or expired, Please log in again.")
             store_id = await getStoreIdByUserId(user_id);
-        }else throw new Error("Access Denied")
+        } else throw new Error("Access Denied")
 
         const { id, status } = req.body;
         //! Validar Algo
 
-        const result  = await orderPatchController( id, store_id, status );
-        return res.status(200).json( result )
+        const result = await orderPatchController(id, store_id, status);
+        return res.status(200).json(result)
     } catch (error) {
         return res.status(400).json({ error: error.message })
     }
@@ -76,13 +76,13 @@ const processOrderGet = async (req, res) => {
         const origin = req.headers.origin;
         let store_id = "";
         let user_email = "";
-        if ( origin === process.env.HEADERS_STORE_ORIGIN_DEPLOY){
+        if (origin === process.env.HEADERS_STORE_ORIGIN_DEPLOY) {
             const token = req.headers.token;
             const user_id = req.headers.id;
-            if ( !token )  throw Error('AccessToken doesnt exist');
-            if ( !await validateToken(user_id, token ) ) throw Error("Token is invalid or expired, Please log in again.")
+            if (!token) throw Error('AccessToken doesnt exist');
+            if (!await validateToken(user_id, token)) throw Error("Token is invalid or expired, Please log in again.")
             store_id = await getStoreIdByUserId(user_id);
-        }else if ( origin === process.env.HEADERS_CUSTOMER_ORIGIN_DEPLOY){
+        } else if (origin === process.env.HEADERS_CUSTOMER_ORIGIN_DEPLOY) {
             const short_name = req.headers.storename;
             user_email = req.headers.user_email;
 
@@ -92,27 +92,28 @@ const processOrderGet = async (req, res) => {
 
 
         const result = user_email.length
-                    ? await orderGetController(store_id, user_email)
-                    : await orderGetController(store_id)
+            ? await orderGetController(store_id, user_email)
+            : await orderGetController(store_id)
 
-        return res.status(200).json( result )
+        return res.status(200).json(result)
     } catch (error) {
         return res.status(400).json({ error: error.message })
     }
 }
-const processOrderGetById= async (req, res) => {
+const processOrderGetById = async (req, res) => {
     try {
         //! Remastered !//
         const origin = req.headers.origin;
         let store_id = "";
         let user_email = "";
-        if ( origin === process.env.HEADERS_STORE_ORIGIN_DEPLOY){
-            const token = req.headers.token;
-            const user_id = req.headers.id;
-            if ( !token )  throw Error('AccessToken doesnt exist');
-            if ( !await validateToken(user_id, token ) ) throw Error("Token is invalid or expired, Please log in again.")
-            store_id = await getStoreIdByUserId(3);
-        }else if ( origin ===process.env.HEADERS_CUSTOMER_ORIGIN_DEPLOY){
+        const token = req.headers.token;
+        const user_id = req.headers.id;
+        
+        if (origin === process.env.HEADERS_STORE_ORIGIN_DEPLOY) {
+            if (!token) throw Error('AccessToken doesnt exist');
+            if (!await validateToken(user_id, token)) throw Error("Token is invalid or expired, Please log in again.")
+            store_id = await getStoreIdByUserId(user_id);
+        } else if (origin === process.env.HEADERS_CUSTOMER_ORIGIN_DEPLOY) {
             const short_name = req.headers.storename;
             user_email = req.headers.user_email;
 
@@ -121,8 +122,8 @@ const processOrderGetById= async (req, res) => {
         else throw new Error("Access Denied")
 
         const { id } = req.params;
-        const result  = await orderGetByIdController(id, store_id, email)
-        return res.status(200).json( result )
+        const result = await orderGetByIdController(id, store_id, email)
+        return res.status(200).json(result)
     } catch (error) {
         return res.status(400).json({ error: error.message })
     }
